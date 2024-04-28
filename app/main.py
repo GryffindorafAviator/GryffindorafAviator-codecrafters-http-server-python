5580411777c3538173879a8b677bf674656daa58
gryffindoraf <phynew0111@gmail.com>
2024-04-28 18:59:55 -0400

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -1,34 +1,40 @@
 import re
 import socket
+import threading
 
 
 def main():
     print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server listening on localhost:4221")
+    while True:
+        client_socket, client_address = server_socket.accept()  # wait for client
+        print(f"Connection from {client_address}")
+        client_thread = threading.Thread(target=handle_client(), args=(client_socket,))
+        client_thread.start()
 
-    client_socket, client_address = server_socket.accept()  # wait for client
-    print(f"Connection from {client_address}")
 
-    request_data = client_socket.recv(1024).decode()
-    lines = request_data.split('\r\n')
-    method, path, http_version = lines[0].split()
-
-    if path == '/':
-        http_response = "HTTP/1.1 200 OK\r\n\r\n"
-    elif path is not None and path.find("/echo") == 0:
-        random_string = extract_random_string(request_data)
-        http_response = prepare_response(random_string)
-    elif path is not None and path.find("/user-agent") == 0:
-        _, user_agent = lines[2].split()
-        res_string = user_agent
-        http_response = prepare_response(res_string)
-    else:
-        http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
-
-    client_socket.sendall(http_response.encode())
-    client_socket.close()
+def handle_client(client_socket):
+    try:
+        request_data = client_socket.recv(1024).decode()
+        lines = request_data.split('\r\n')
+        method, path, http_version = lines[0].split()
+        if path == '/':
+            http_response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif path is not None and path.find("/echo") == 0:
+            random_string = extract_random_string(request_data)
+            http_response = prepare_response(random_string)
+        elif path is not None and path.find("/user-agent") == 0:
+            _, user_agent = lines[2].split()
+            res_string = user_agent
+            http_response = prepare_response(res_string)
+        else:
+            http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        client_socket.sendall(http_response.encode())
+    except Exception as e:
+        print(f"Error handling client: {e}")
+    finally:
+        client_socket.close()
 
 
 def extract_random_string(request_data):
