532a7473c1ae4bf392adad783c8baed4c5edbf73
gryffindoraf <phynew0111@gmail.com>
2024-04-28 11:40:05 -0400

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
 import socket
 
+
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server listening on localhost:4221")
 
-    client_socket, client_address = server_socket.accept() # wait for client
+    client_socket, client_address = server_socket.accept()  # wait for client
     print(f"Connection from {client_address}")
 
-    # HTTP response with status line and empty headers
-    http_response = "HTTP/1.1 200 OK\r\n\r\n"
+    request_data = client_socket.recv(1024).decode()
+    path = extract_path(request_data)
 
-    # Send the HTTP response
+    if path == '/':
+        http_response = "HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+
     client_socket.sendall(http_response.encode())
-
-    # Close the connection
     client_socket.close()
+
+
+def extract_path(request_data):
+    lines = request_data.split('\r\n')
+    start_line = lines[0]
+    method, path, http_version = start_line.split()
+    return path
+
 
 if __name__ == "__main__":
     main()
