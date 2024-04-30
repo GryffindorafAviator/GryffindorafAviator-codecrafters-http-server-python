416e74c2b3a535e0fb181f4cf46fd04c8c1ef910
gryffindoraf <phynew0111@gmail.com>
2024-04-30 19:42:38 -0400

    pass 8th stage

--- app/main.py
+++ app/main.py
@@ -19,36 +19,37 @@ def main():
 def handle_client(client_socket):
     try:
         request_data = client_socket.recv(1024).decode()
-        lines = request_data.split('\r\n')
+        request_header, request_body = request_data.split('\r\n\r\n', 1)
+        lines = request_header.split('\r\n')
         method, path, http_version = lines[0].split()
         if path == '/':
             http_response = "HTTP/1.1 200 OK\r\n\r\n"
-            # client_socket.sendall(http_response.encode())
         elif path.startswith("/echo"):
             random_string = extract_random_string(request_data)
             http_response = prepare_response(random_string)
-            # client_socket.sendall(http_response.encode())
         elif path.startswith("/user-agent"):
             _, user_agent = lines[2].split()
             res_string = user_agent
             http_response = prepare_response(res_string)
-            # client_socket.sendall(http_response.encode())
         elif path.startswith("/files"):
             file_path = path.lstrip('/files/')
             os.chdir(sys.argv[2])
-            contents = ""
-            files = os.listdir()
-            if file_path in files:
-                with open(file_path, 'r') as f:
-                    contents = f.read()
-            if contents:
-                http_response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:{len(contents)}\r\n\n{contents}\r\n\r\n"
+            if method == "GET":
+                contents = ""
+                files = os.listdir()
+                if file_path in files:
+                    with open(file_path, 'r') as f:
+                        contents = f.read()
+                if contents:
+                    http_response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length:{len(contents)}\r\n\n{contents}\r\n\r\n"
+                else:
+                    http_response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
             else:
-                http_response = "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-            # client_socket.sendall(http_response)
+                with open(file_path, 'w') as f:
+                    f.write(request_body)
+                    http_response = "HTTP/1.1 201 Created\r\n\r\n"
         else:
             http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
-            # client_socket.sendall(http_response.encode())
         client_socket.sendall(http_response.encode())
     except Exception as e:
         print(f"Error handling client: {e}")
@@ -62,12 +63,6 @@ def extract_random_string(request_data):
         return random_string.group(1)
     else:
         return None
-
-
-# def prepare_response2(status_code, body, content_type):
-#     status_line = f"HTTP/1.1 {status_code}\r\n"
-#     headers = f"Content-Type: {content_type}\r\nContent-Length: {len(body)}\r\n\r\n"
-#     return status_line.encode() + headers.encode() + body
 
 
 def prepare_response(res_string):
